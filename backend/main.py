e2956fb56abb418b866fb3d3d08dab193c75866b
Jean-Patrick Debaëne <jean-patrick.debaene@sa-cim.fr>
2022-08-18 17:26:20 +0200

    feat :  - add new endpoint that returns user's balance  - update pytest version (7.1.2)

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 
 from backend.db import InMemoryDB
 from backend.logic import transactions
-from backend.models import Transaction
+from backend.models import Transaction, TransactionState, TransactionType
 
 app = FastAPI()
 
@@ -21,6 +21,64 @@ async def get_transactions(user_id: int):
     return transactions.transactions(db, user_id)
 
 
+@app.get("/users/{user_id}/transactions/balance")
+async def get_balance(user_id: int):
+    """Computes the balance of payments for a user subscription."""
+    users_transactions = [t for t in transactions.transactions(db, user_id)]
+
+    sums_deposits = sum(
+        [
+            t.amount
+            for t in users_transactions
+            if t.type == TransactionType.DEPOSIT
+            and t.state == TransactionState.COMPLETED
+        ]
+    )
+    sums_withdrawals = sum(
+        [
+            t.amount
+            for t in users_transactions
+            if t.type == TransactionType.SCHEDULED_WITHDRAWAL
+            and t.state == TransactionState.COMPLETED
+        ]
+    )
+    sums_refund = sum(
+        [
+            t.amount
+            for t in users_transactions
+            if t.type == TransactionType.REFUND
+            and t.state in [TransactionState.COMPLETED, TransactionState.PENDING]
+        ]
+    )
+
+    balance = sums_deposits - sums_withdrawals - sums_refund
+
+    scheduled_withdrawals = [
+        t
+        for t in users_transactions
+        if t.type == TransactionType.SCHEDULED_WITHDRAWAL
+        and t.state == TransactionState.SCHEDULED
+    ]
+
+    withdrawals = []
+
+    for transaction in sorted(scheduled_withdrawals, key=lambda t: t.date):
+        coverage = min(round((balance / transaction.amount) * 100, 2), 100)
+        coverage_amount = min(transaction.amount, balance)
+        balance = max(balance - transaction.amount, 0)
+
+        withdrawals.append(
+            {
+                "amount": transaction.amount,
+                "coverage": coverage,
+                "coverage_amount": coverage_amount,
+                "date": transaction.date,
+            }
+        )
+
+    return {"withdrawals": withdrawals, "balance": balance}
+
+
 @app.get("/users/{user_id}/transactions/{transaction_id}")
 async def get_transaction(user_id: int, transaction_id: int):
     """Returns a given transaction of the user."""
@@ -34,10 +92,3 @@ async def get_transaction(user_id: int, transaction_id: int):
 async def create_transaction(user_id: int, transaction: Transaction):
     """Adds a new transaction to the list of user transactions."""
     return transactions.create_transaction(db, user_id, transaction)
-
-
-@app.get("/users/{user_id}/transactions/balance")
-async def get_balance(user_id: int):  # pylint: disable=unused-argument
-    """Computes the balance of payments for a user subscription."""
-    # We expect you to write this function
-    return None
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -7,25 +7,29 @@ from backend.main import app
 
 client = TestClient(app)
 
+
 @pytest.fixture
 def deposit_transaction():
     return {
         "amount": 10.5,
         "type": "deposit",
-        "date": date.today().strftime("%Y-%m-%d")
+        "date": date.today().strftime("%Y-%m-%d"),
     }
+
 
 def test_hello():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
+
 def test_get_transactions():
     response = client.get("users/1/transactions")
     assert response.status_code == 200
     for transaction in response.json():
         assert transaction["user_id"] == 1
-    
+
+
 def test_get_existing_transaction():
     response = client.get("users/1/transactions/1")
     assert response.status_code == 200
@@ -33,9 +37,11 @@ def test_get_existing_transaction():
     assert transaction["user_id"] == 1
     assert transaction["id"] == 1
 
+
 def test_get_nonexisting_transaction():
     response = client.get("users/1/transactions/9999")
     assert response.status_code == 404
+
 
 def test_create_transaction(deposit_transaction):
     response = client.post("users/2/transactions", json=deposit_transaction)
@@ -46,3 +52,28 @@ def test_create_transaction(deposit_transaction):
     assert transaction["type"] == "deposit"
     assert transaction["date"] == date.today().isoformat()
     assert transaction["state"] == "pending"
+
+
+def test_get_balance():
+    response = client.get("/users/1/transactions/balance")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["balance"] == 0
+    assert len(data["withdrawals"]) == 3
+    assert data["withdrawals"][0]["amount"] == 20
+    assert data["withdrawals"][1]["amount"] == 20
+    assert data["withdrawals"][2]["amount"] == 300
+    assert data["withdrawals"][0]["coverage"] == 100
+    assert data["withdrawals"][1]["coverage"] == 100
+    assert data["withdrawals"][2]["coverage"] == 5.67
+    assert data["withdrawals"][0]["coverage_amount"] == 20
+    assert data["withdrawals"][1]["coverage_amount"] == 20
+    assert data["withdrawals"][2]["coverage_amount"] == 17
+
+
+def test_get_balance_user_not_exist():
+    response = client.get("/users/111/transactions/balance")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["balance"] == 0
+    assert len(data["withdrawals"]) == 0
